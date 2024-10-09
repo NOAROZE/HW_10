9894d2c3d5aa6c20b1cbb27ba0c30b40481f41d4
NOAROZE <156937034+NOAROZE@users.noreply.github.com>
2024-10-09 17:30:10 +0300

    Update num_1.py

--- num_1.py
+++ num_1.py
@@ -5,7 +5,6 @@ numbers1: list[int] = [i for i in range(10, 22, 2)]
 list_random: list[bool] = [random.choice(True, False) for _ in range(5)]
 list_random1: list[int] = [random.randint(1, 100) for _ in range(10)]
 list_random2: list[int] = [random.randint for random.randint in list_random1 if random.randint > 50]
-# list_random3: list[int] = [random.randint for _ in range(10)random.randint for random.randint in list_random if random.randint > 50]
 list_random4: list[int] = [random.randint(10, 100) for _ in range(10)]
 print(list_random4)
 new_list: list[int] = [random.randint % 10 for random.randint in list_random4]
